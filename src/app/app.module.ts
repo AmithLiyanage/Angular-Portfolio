7289a3344143fb5f51987362da52c5de72a87f6f
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-06-21 21:09:15 +0530

    maked 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { BlogComponent } from './blog/blog.component';
+import { BoxComponent } from './box/box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
